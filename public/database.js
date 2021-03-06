447681f8086727c67d6a30420245517a34e9ac01
smedigo <smedigo@gmail.com>
2021-03-05 19:35:54 -0800

    adding checking the BD when back online

--- public/database.js
+++ public/database.js
@@ -19,3 +19,48 @@ request.onupgradeneeded = (event) => {
   request.onerror = (event) => {
     console.log(event.target.errorCode);
   };
+
+  // save records
+  const saveRecord=(record) => {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+
+  };
+
+  // check DB when back online
+
+  const checkDatabase = () => {
+    // open a transaction on your pending db
+    const transaction = db.transaction(["pending"], "readwrite");
+    // access your pending object store
+    const Store = transaction.objectStore("pending");
+    // get records from store
+    const getAll = pendingStore.getAll();
+  
+    getAll.onsuccess = async () => {
+        if (getAll.result.length > 0) {
+          fetch("/api/transaction/bulk", {
+              method: "POST",
+              body: JSON.stringify(getAll.result),
+              headers: {
+                  Accept: "application/json, text/plain, */*",
+                  "Content-Type": "application/json"
+              }
+          });
+  
+          await res.json();
+  
+          // open a transaction on the pending db
+          const transaction = db.transaction(["pending"], "readwrite");
+  
+          // access the store
+          const Store = transaction.objectStore("pending");
+  
+          // Clear all items in the store
+          Store.clear()
+        };
+    };
+  };
+  
+  window.addEventListener("online", checkDatabase);
